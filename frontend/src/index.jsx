b33c3d144ac4d848067f2888745d7e2c8359688f
Dev Moreido <107591449+devmoreido@users.noreply.github.com>
2023-03-12 18:33:46 -0300

    Replace App component by ToDoPage component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyle from './styles/globalStyle';
+import ToDoPage from './pages/todo/ToDoPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <ToDoPage />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ frontend/src/pages/todo/ToDoPage.jsx
@@ -0,0 +1,5 @@
+function ToDoPage() {
+    return <h1>To Do Page!</h1>;
+}
+
+export default ToDoPage;
