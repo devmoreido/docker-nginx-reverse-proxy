d2bef3c3d3d73e5a32160d448eb002f1d354fac0
Dev Moreido <107591449+devmoreido@users.noreply.github.com>
2023-03-12 19:20:38 -0300

    Add functions to create and clear tasks

--- frontend/src/pages/todo/ToDoPage.jsx
+++ frontend/src/pages/todo/ToDoPage.jsx
@@ -3,14 +3,37 @@ import NewTaskForm from '../../components/NewTaskForm';
 import MainSection from '../../components/MainSection';
 import HeaderSection from '../../components/HeaderSection';
 import ClearAllTasks from '../../components/ClearAllTasks'
+import { useEffect, useState } from 'react';
 
 function ToDoPage() {
+    const [tasks, setTasks] = useState([]);
+    const [hasTasks, setHasTasks] = useState(false);
+
+    useEffect(() => setHasTasks(!!tasks.length), [tasks]);
+
+    const createTask = description => ({
+        id: tasks.length + 1,
+        isDone: false,
+        description
+    });
+
+    const addNewTask = description => {
+        const newTask = createTask(description);
+        setTasks([...tasks, newTask]);
+    }
+
+    const clearAllTasks = () => {
+        if(window.confirm('Are you sure you want to remove all tasks?')) {
+            setTasks([]);
+        }
+    }
+    
     return <MainSection>
         <HeaderSection>
             <PageTitle>📖 My daily tasks</PageTitle>
-            <ClearAllTasks />
+            <ClearAllTasks visibleWhen={hasTasks} onClear={clearAllTasks} />
         </HeaderSection>
-        <NewTaskForm />
+        <NewTaskForm onConfirm={addNewTask} />
     </MainSection>;
 }
 
