153af4e96f64d89e1bb215a3325eba4005f3efea
Dev Moreido <107591449+devmoreido@users.noreply.github.com>
2023-03-12 18:22:19 -0300

    Add style theme

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+    colors: {
+        body: '#ffffff',
+        primary: '#217373',
+        secondary: '#ebf2f9',
+        secondaryDark: '#e4edf7',
+        success: '#74af95',
+        successDark: '#5da284',
+        danger: '#da5858',
+        dangerDark: '#b02626',
+        text: '#173540',
+        textDark: '#161f30',
+        textLight: '#ebf2f9'
+    },
+    borders: {
+        radius: '4px'
+    },
+    fonts: {
+        body: '"Quicksand", sans-serif',
+    },
+};
+
+export default theme;
